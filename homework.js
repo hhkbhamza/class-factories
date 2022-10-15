ec57b00817621bdeef8637faee1f7c9f535f4945
Amir Hamza <a.hamza8596@gmail.com>
2022-10-14 23:03:28 -0400

    created person class

--- homework.js
+++ homework.js
@@ -1,8 +1,8 @@
 class Hamster {
-    constructor(owner, name, price) {
-        this.owner = owner
+    constructor(name) {
+        this.owner = ""
         this.name = name
-        this.price = price
+        this.price = 15
     }
     wheelRun() {
         console.log("squeak squeak")
@@ -14,8 +14,58 @@ class Hamster {
         return this.price
     }
 }
-const h = new Hamster("", "Tom", 15)
+const h = new Hamster("Tom")
 h.wheelRun()
 h.eatFood()
 console.log(h.getPrice())
 console.log(h)
+
+
+class Person {
+    constructor(name) {
+        this.name = name
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamsters = []
+        this.bankAccount = 0
+    }
+    getName() {
+        return this.name
+    }
+    getAge() {
+        return this.age
+    }
+    getWeight() {
+        return this.weight
+    }
+    greet() {
+        console.log("Hello " + this.name)
+    }
+    eat() {
+        this.weight++
+        this.mood++
+    }
+    exercise() {
+        this.weight--
+    }
+    ageUp() {
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount += 10
+    }
+    buyHamster(pet) {
+        p.hamsters.push(pet)
+        this.mood += 10
+        this.bankAccount = this.bankAccount -= pet.getPrice()
+    }
+}
+const p = new Person("Henry")
+p.buyHamster(h)
+console.log(p)
+
+
+const timmy = new Person("Timmy")
